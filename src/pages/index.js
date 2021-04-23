5273f85f8a765b034c47aafc5cfdb32908166b9d
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-04-23 12:59:43 +0500

    add: js for select changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,14 @@
-import './index.scss';
+import "./index.scss";
+
+const selects = document.querySelectorAll(".logistics__label-input-select");
+
+selects.forEach((select) => {
+  select.addEventListener("change", (evt) => {
+    const defaultOption = evt.target.querySelector(
+      ".logistics__label-select-option_default"
+    );
+    if (defaultOption.textContent !== evt.target.value)
+      evt.target.style.color = "#000";
+    else evt.target.style.color = "#cfcfcf";
+  });
+});
