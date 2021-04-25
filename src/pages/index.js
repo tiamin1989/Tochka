a9df4c55bb2c0b91c3950fe26cfc6769f11a590c
Ruslan Ramazanov <tiamin1989@yandex.ru>
2021-04-25 23:48:09 +0500

    select width onblur features

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,41 @@ import "./index.scss";
 
 const selects = document.querySelectorAll(".logistics__label-input-select");
 
-const checkOption = (select) => {
+const textWrapper = document.querySelector(".page__text-length");
+
+const checkSelectColor = (select) => {
   const defaultOption = select.querySelector(
     ".logistics__label-select-option_default"
   );
   if (defaultOption.textContent !== select.value)
     select.style.color = "#000";
-  else select.style.color = "#cfcfcf";
+  else {
+    select.style.color = "#cfcfcf";
+  }
+  select.size = "1";
+  select.blur();
+}
+
+const selectOnFocus = (select, optionsCount) => {
+  select.size = `${optionsCount}`;
+  select.style.position = "relative";
+  select.style.zIndex = "1";
+  select.style.width = "max-content";
+}
+
+const selectOnBlur = (select, selectOffsetWidth) => {
+  select.size = "0";
+  select.style.position = "unset";
+  select.style.zIndex = "0";
+  select.style.width = `${selectOffsetWidth}px`;
 }
 
 selects.forEach((select) => {
-  checkOption(select);
-  select.addEventListener("change", (evt) => checkOption(evt.target));
+  const selectOffsetWidth = select.offsetWidth;
+  const options = select.querySelectorAll('option');
+
+  checkSelectColor(select);
+  select.addEventListener("change", (evt) => checkSelectColor(select));
+  select.addEventListener("focus", (evt) => selectOnFocus(select, options.length));
+  select.addEventListener("blur", (evt) => selectOnBlur(select, selectOffsetWidth));
 });
